2acd5b0609d33ad7ff47feeb4c4d49c5a694c921
anantkumar123347 <anibaliyan111@gmail.com>
2024-10-14 12:53:16 +0530

    jswork

---
+++ jswork/index.js
@@ -0,0 +1,33 @@
+/*console.log("Hello Anant")
+let a=10
+if(a>10)
+{
+    a=20
+console.log(a)
+}
+console.log(typeof(a))
+let a=(x,y)=>{
+    console.log("hello Anant")
+    return x+y
+}
+let n=a(10,20)
+console.log(n)*/
+function selectlang(clk)
+{
+    if(clk=="c")
+    {
+        return function  comp()
+{
+    return "c"
+}
+    }
+else
+{
+    return function jomp()
+{
+    return "java"
+}
+}
+    console.log("language= "+clk)
+}
+selectlang("c")
